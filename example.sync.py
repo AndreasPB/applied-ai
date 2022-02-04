976b4771e98ad6ddca55578e1be7747c93983073
AndreasPB <andreas.p.brodersen@gmail.com>
2022-02-04 15:26:35 +0100

    Init commit - Poetry + Jupyter notebook setup

---
+++ example.sync.py
@@ -0,0 +1,14 @@
+# %% [markdown]
+# # Fed titel!
+
+# %%
+import time
+import tqdm
+
+# %% [markdown]
+# ## Meeeeget vigtigt loop
+
+# %%
+for _ in tqdm.trange(10, desc="Loader ingenting 8-)"):
+    time.sleep(0.5)
+
